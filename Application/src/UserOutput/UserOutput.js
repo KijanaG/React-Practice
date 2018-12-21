c3730736346316650482cad282cb788b376e3c97
KJ_Garrett <kijana.garrett@gmail.com>
2018-12-21 13:34:08 -0800

    Prior to first Assignment

---
+++ Application/src/UserOutput/UserOutput.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const userOutput = ( props ) => {
+    return (
+        <div>
+            <h2> Username: {} </h2>
+            <p>
+                The quick brown fox jumped over the rest of this phrase, turned around, 
+                smacked it for being what it is and dipped quickly. So quickly in fact
+                that they replaced this ridiculous phrase with a new and outstanding "QUOTE" 
+            </p>
+            <p>
+                The quick brown fox jumped over the rest of this phrase, turned around, 
+                smacked it for being what it is and dipped quickly. So quickly in fact
+                that they replaced this ridiculous phrase with a new and outstanding "QUOTE" 
+            </p>
+        </div>
+    );
+}
+
+export default userOutput;
